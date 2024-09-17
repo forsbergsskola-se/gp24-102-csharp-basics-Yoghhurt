5dd3a4b4f5a2e46de92a117833dc32111f249366
JackBillingham <billingham.jacks@gmail.com>
2024-09-17 15:04:22 +0200

    P8

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Give me a number.");
+
+string input = Console.ReadLine();
+float numberOne = float.Parse(input);
+
+Console.WriteLine(numberOne);
+
+int numberTwo = (int)numberOne;
+string finalNumber = numberTwo.ToString();
+
+Console.WriteLine(finalNumber);
