c23c78f470b07c89fa361fde48d9b7b8ab4879ec
JackBillingham <billingham.jacks@gmail.com>
2024-09-17 15:52:02 +0200

    saving for today, p10

---
+++ P10Boolean/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your age?"); 
+
+int age = Console.ReadLine();
+
+
+
+bool isChild = 0 <= 12;
+bool isTeen = 13 <= 19;
+bool isAdult = 19 > 1000;
+
+
+if (isChild)
+{
+    Console.WriteLine("Youre a child:"); 
+}
+
+if (isTeen)
+{
+    Console.WriteLine("Youre a teen:");
+}
+if (isAdult) {
+Console.WriteLine("Youre an Adult:");
+}
