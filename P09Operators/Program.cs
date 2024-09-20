f32fc1f6f32797657fe086c01a419b59eb12ff74
JackBillingham <billingham.jacks@gmail.com>
2024-09-20 10:23:17 +0200

    P9

---
+++ P09Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Give me a number of seconds");
+string userInput = Console.ReadLine();
+
+int seconds = int.Parse(userInput);
+
+int numberOfDays = seconds / (24 * 3600);
+int remainingSeconds = seconds % (24 * 3600);
+int numberOfHours = remainingSeconds / 3600;
+remainingSeconds %= 3600;
+int numberOfMinutes = remainingSeconds / 60;
+int numberOfSeconds = remainingSeconds % 60;
+
+Console.WriteLine("Number of days " + numberOfDays);
+Console.WriteLine("Number of hours " + numberOfHours);
+Console.WriteLine("Number of minutes " + numberOfMinutes);
+Console.WriteLine("Number of seconds " + numberOfSeconds);
+
+Console.WriteLine($"{numberOfDays}.{numberOfHours}:{numberOfMinutes}:{numberOfSeconds}");
+
+Console.WriteLine($"In total that is {(float)seconds / 86400} days.");
