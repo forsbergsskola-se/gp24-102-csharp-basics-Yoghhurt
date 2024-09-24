8856e402154e3417d3fd4dd91ef8be97c2b1a1ad
JackBillingham <billingham.jacks@gmail.com>
2024-09-24 12:02:16 +0200

    P11_1

--- P11Ifelse/Program.cs
+++ P11Ifelse/Program.cs
@@ -4,3 +4,29 @@ Console.WriteLine("Let's check your grade");
 string result = Console.ReadLine();
 
 int result2 = int.Parse(result);
+
+if (result2 >= 90)
+{Console.WriteLine("Your grade is: A");
+    
+}
+
+if (result2 >= 80)
+{Console.WriteLine("Your grade is: B");
+    
+}
+
+if (result2 >= 70)
+{Console.WriteLine("Your grade is: C");
+    
+}
+
+if (result2 >=60)
+{Console.WriteLine("Your grade is: D");
+    
+}
+else
+{
+    Console.WriteLine("Your grade is: F");
+}
+
+//P11_2
